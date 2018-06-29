9bbe1eed237859ba42865385edd9bb0de34f1752
tripott <tripott@gmail.com>
2018-06-29 11:00:16 -0400

    test changes

--- dal.js
+++ dal.js
@@ -8,11 +8,11 @@ const pkGen = require('./lib/pk-gen')
 const db = new PouchDB(`${process.env.COUCHDB_URL}${process.env.COUCHDB_NAME}`)
 
 const addPainting = (painting, callback) => {
-	const modifiedPainting = merge(painting, {
-		type: 'painting',
-		_id: pkGen('painting', '_', `${painting.name}`)
-	})
-	db.put(modifiedPainting, callback)
+  const modifiedPainting = merge(painting, {
+    type: 'painting',
+    _id: pkGen('painting', '_', `${painting.name}`)
+  })
+  db.put(modifiedPainting, callback)
 }
 
 const getPainting = (id, callback) => db.get(id, callback)
@@ -20,24 +20,24 @@ const getPainting = (id, callback) => db.get(id, callback)
 const updatePainting = (painting, callback) => db.put(painting, callback)
 
 const deletePainting = (id, callback) => {
-	db.get(id, function(err, painting) {
-		db.remove(painting, callback)
-	})
+  db.get(id, function(err, painting) {
+    db.remove(painting, callback)
+  })
 }
 
 const listPaintings = (limit, paginate) =>
-	db
-		.allDocs(
-			paginate
-				? { include_docs: true, limit, lastItem: `${paginate}\ufff0` }
-				: { include_docs: true, limit }
-		)
-		.then(response => map(prop('doc'), response.rows))
+  db
+    .allDocs(
+      paginate
+        ? { include_docs: true, limit, start_key: `${paginate}\ufff0` }
+        : { include_docs: true, limit }
+    )
+    .then(response => map(prop('doc'), response.rows))
 
 module.exports = {
-	addPainting,
-	getPainting,
-	updatePainting,
-	deletePainting,
-	listPaintings
+  addPainting,
+  getPainting,
+  updatePainting,
+  deletePainting,
+  listPaintings
 }
